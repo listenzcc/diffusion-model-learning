9138fd8180453c1e28d225aa83be2e2488050636
listenzcc <listenzcc@mail.bnu.edu.cn>
2023-05-25 11:01:22 +0800

    Add readme II

--- draw-with-mpl/draw_with_mpl.py
+++ draw-with-mpl/draw_with_mpl.py
@@ -91,49 +91,81 @@ cmap
 
 # %%
 
-sigma = 1.0
-mu = np.repeat(signal[np.newaxis, :], RANDOM_POPULATION, axis=0)
-data = mu + np.random.randn(RANDOM_POPULATION, POINTS) * sigma
-distance = [np.linalg.norm(d - signal) for d in data]
+
+def draw_signal_with_I_cov(signal: np.array, sigma: float, ax: plt.Axes, use_mu_flag=True, legend_flag=True):
+    """
+    Draw the signal with I covariance matrix.
+
+    Args:
+        signal (np.Array): The signal to draw in red line, and the noise are added by sigma.
+        sigma (float): The strength of the variance.
+        ax (plt.Axes): The axes to draw the signal with.
+        use_mu_flag (bool): Whether to draw the signal's noise by its mu. Default is True.
+        legend_flag (bool): Whether to draw the legend. Defaults to True.
+
+    Returns:
+        data: The generated signal plus noise.
+    """
+
+    mu = np.repeat(signal[np.newaxis, :], RANDOM_POPULATION, axis=0)
+
+    if not use_mu_flag:
+        mu *= 0
+
+    data = mu + np.random.randn(RANDOM_POPULATION, POINTS) * sigma
+    distance = [np.linalg.norm(d - signal) for d in data]
+
+    def sort_by(iterable, by, reverse=False):
+        by = np.array(by)
+        ind = np.argsort(by)
+        if reverse:
+            ind = ind[::-1]
+        return iterable[ind], by[ind]
+
+    def opacity(color, opacity=0.5):
+        color = list(color)
+        if len(color) == 3:
+            color.append(opacity)
+        else:
+            color[3] = opacity
+        return tuple(color)
+
+    data, distance = sort_by(data, distance, reverse=True)
+
+    norm = mpl.colors.Normalize(vmin=np.min(distance), vmax=np.max(distance))
+    for y, d in zip(data, distance):
+        ax.plot(y, color=opacity(cmap(norm(d)), 0.5))
+    # The latest drawn line is on the most top
+    ax.plot(signal, color='red', label='Signal')
+
+    plt.colorbar(plt.cm.ScalarMappable(cmap=cmap, norm=norm), ax=ax)
+
+    if legend_flag:
+        ax.legend(loc='lower right')
+
+    ax.set_facecolor('gray')
+    ax.set_title('Signal and its noise: (sigma={})'.format(sigma))
+
+    return data
 
 
-def sort_by(iterable, by, reverse=False):
-    by = np.array(by)
-    ind = np.argsort(by)
-
-    if reverse:
-        ind = ind[::-1]
-
-    return iterable[ind], by[ind]
-
-
-def opacity(color, opacity=0.5):
-    color = list(color)
-    if len(color) == 3:
-        color.append(opacity)
-    else:
-        color[3] = opacity
-    return tuple(color)
-
-
-data, distance = sort_by(data, distance, reverse=True)
-
-# Draw the data
-fig, ax = plt.subplots(figsize=(6, 3))
-
-norm = mpl.colors.Normalize(vmin=np.min(distance), vmax=np.max(distance))
-for y, d in zip(data, distance):
-    ax.plot(y, color=opacity(cmap(norm(d)), 0.5))
-
-# The latest drawn line is on the most top
-ax.plot(signal, color='red', label='Signal')
-
-plt.colorbar(plt.cm.ScalarMappable(cmap=cmap, norm=norm), ax=ax)
-ax.legend()
-ax.set_facecolor('gray')
-ax.set_title('Signal and its noise')
-
+# Draw the signal with its mu
+fig, axs = plt.subplots(3, 1, figsize=(6, 6))
+draw_signal_with_I_cov(signal, 1.0, axs[0], legend_flag=False)
+draw_signal_with_I_cov(signal, 0.5, axs[1], legend_flag=False)
+draw_signal_with_I_cov(signal, 0.1, axs[2], legend_flag=True)
 fig.tight_layout()
 plt.show()
+
+# Draw the signal without its mu
+fig, axs = plt.subplots(3, 1, figsize=(6, 3))
+draw_signal_with_I_cov(signal, 0.1, axs[2], use_mu_flag=False)
+fig.tight_layout()
+plt.show()
+
+# %%
+im = plt.imshow(signal.reshape(10, 10))
+plt.title('Signal in rect')
+plt.colorbar(im)
 
 # %%
